980fb906c508f78d985c50439cc43cbcc28ba078
mahorohope <mahorohope56@gmail.com>
2025-12-11 23:45:16 -0800

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(3000, () => {
+server.listen(3000, '0.0.0.0', () => {
   console.log('Server running at http://localhost:3000/');
 });
