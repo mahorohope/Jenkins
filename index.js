26ef2b551439b59467435677f26220c3dfdb1b49
mahorohope <mahorohope56@gmail.com>
2025-12-11 23:44:27 -0800

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/plain');
     res.end('About Page.\n');
   } else {
-    res.statusCode = 3000;
+    res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello, World!\n');
   }
